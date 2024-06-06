b4cf1507f9c113419725c35f9fc43829403b8048
Kevin-Itam <kevinitamar16@gmail.com>
2024-06-06 20:53:37 -0300

    06/06/2024

--- primeiro_projeto_react/src/components/pages/Contact.jsx
+++ primeiro_projeto_react/src/components/pages/Contact.jsx
@@ -1,6 +1,18 @@
+import styles from './Contact.module.css'
+
 function Contact(){
     return(
-        <h1>Contact</h1>
+        <section className={styles.contact_container}>
+            <h1>Nos Contate</h1>
+            <div>
+                <form action="">
+                    <input type="text" name="Nome" id="Nome" placeholder='Seu nome ...' />
+                    <input type="text" name="Tel" id="Tel" placeholder='Seu telefone ...' />
+                    <input type="email" name="email" id="email" placeholder='Seu Email aqui ...'/>
+                </form>
+            </div>
+        </section>
+
     )
 
 }
